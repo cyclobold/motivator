8ee3c46979ffd1bf307d911cadd185b593ed49a4
Olu <theoafactor@gmail.com>
2020-09-22 18:21:32 +0100

    Added Create Profile Form

--- js/profile.js
+++ js/profile.js
@@ -4,45 +4,55 @@
  * Opens the Create Profile Form
  */
 
-const create_profile_button = document.getElementById("create-profile-id");
 
-create_profile_button.addEventListener("click", function(){
-		
+
+function test(){
 		//brings up the form.. 
-		
+		const create_profile_button = document.getElementById("create-profile-id");
+		const oldContent = JSON.stringify(create_profile_button.parentElement.innerHTML);
+
 		let formCode = `
-						<div>
-						<form class=''>
-							<div>
+						<div class='form-box'>
+						
+							<button class='close-modal btn-cancel-sm btn-transparent' onclick='return cancelCreateProfile(${oldContent})'>Cancel</button>
+						<form class='form'>
+							<h2>Create Account</h2>
+							<div class='form-group'>
 								<label>First name</label>
-								<input type='text' name='firstname' class=''>
+								<input type='text' name='firstname' class='' placeholder='Your First name'>
 							</div>
 
-							<div>
+							<div class='form-group'>
 								<label>Last name</label>
-								<input type='text' name='lastname' class=''>
+								<input type='text' name='lastname' class='' placeholder='Your Last name'>
 							</div>
 
-							<div>
+							<div class='form-group'>
 								<label>Email</label>
-								<input type='email' name='email' class=''>
+								<input type='email' name='email' class='' placeholder='Your Email'>
 							</div>
 
-							<div>
+							<div class='form-group'>
 								<label>Password</label>
-								<input type='password' name='password' class=''>
+								<input type='password' name='password' class='' placeholder='Your Password'>
 							</div>
 
+							<div class='form-group'>
+								<button type='button' class='btn-create-lg'>Create Profile</button>
+							</div>
 
 						</form>
 						</div>`;
 
 
 		//add this form to the page..
-		console.log(create_profile_button.parentElement);
+		create_profile_button.parentElement.innerHTML = formCode;
+
+}
 
 
-
-
-})
-
+function cancelCreateProfile(oldContent){
+	//remove this form.. 
+	document.getElementById("right-navlinks").innerHTML = oldContent;
+	
+}
