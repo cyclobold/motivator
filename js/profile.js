d423714ce7c9deffb8e7fd5eddf3846ea8563b2f
Olu <theoafactor@gmail.com>
2020-10-04 09:44:09 +0100

    - Started working on Tools and Preferences - Added LocalStorage option to make Setting Background in Tools and Preferences - Downloaded new photo by Andreas Galchorn - Still trying to work on the centralizing the page components

--- js/profile.js
+++ js/profile.js
@@ -1,11 +1,46 @@
 "use strict"
 
+
+//Once the page loads ...
+//Check the localStorage for the background Image ..
+//This may need to be cached to prevent the flipping ..
+window.addEventListener("DOMContentLoaded", function(){
+
+	let background_settings = localStorage.getItem('action-background-settings');
+
+	if(background_settings){
+		background_settings = JSON.parse(background_settings);
+		//use the background instead..
+		if(background_settings['show_background'] == true){
+			document.body.style.background = "url('" + background_settings['background_url'] + "')";
+			document.body.style.backgroundRepeat = "no-repeat";
+			document.body.style.backgroundSize = 'cover';
+			document.body.style.backgroundAttachment = "fixed";
+
+		}else{
+			/**
+			background: url('../_thirdparties/images/gurkubondinn/peace.jpg');
+			background-size: cover;
+			background-repeat: no-repeat;
+			background: black
+			color: white;
+			background-attachment: fixed;
+			font-family: 'IBM Plex Mono', monospace;
+			 */
+			document.body.style.background = "#000";
+
+		}
+
+	}else{
+		document.body.style.background = "#000";
+	}
+
+});
+
+
 /**
  * Opens the Create Profile Form
  */
-
-
-
 function test(){
 		//brings up the form.. 
 		const create_profile_button = document.getElementById("create-profile-id");
